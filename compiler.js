96a1f068758b88c45985096d2e520cfc052f6e8d
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2021-08-08 23:23:51 -0400

    Got EJS working so I can implement the static frontend without code duplication, SSI won't work with ExpressJS and I can't be bothered to set up apache.

---
+++ compiler.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const ejs = require("ejs");
+const path = require('path');
+
+
+
+(async ()=>{
+  // Our starting point
+  try {
+    // Get the files as an array
+    const files = await fs.promises.readdir(path.join(__dirname, 'views', 'pages') );
+
+    // Loop them all with the new for...of
+    files.forEach(async (file) => {
+      const templatePath = path.join(__dirname, 'views', 'pages', file);
+      const stat = await fs.promises.stat(templatePath)
+      if(stat.isFile()) {
+        const toPath = path.join(__dirname, 'public', `${file.split('.').slice(0,-1).join('')}.html`)
+        const templateStr = ejs.fileLoader(templatePath, 'utf8');
+        const template = ejs.compile(templateStr, { filename: templatePath });
+        const renderedHtml = template();
+        fs.writeFile(toPath, renderedHtml, function(err) {
+          if(err) { console.log(err); return false }
+          return true;
+        });  
+      }
+    })
+  }
+  catch( e ) {
+    // Catch anything bad that happens
+    console.error( "We've thrown! Whoops!", e );
+  }
+
+})();
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const port = parseInt(process.env.PORT, 10) || 3000;
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+const path = require('path');
+
+
+/**
+ * Live reload setup
+ */
+const liveReloadServer = livereload.createServer()
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+const app = express();
+app.set('view engine', 'ejs');
+
+
+// Use `.hbs` for extensions and find partials in `views/partials`.
+
+app.use(connectLivereload());
+app.use(express.static('public'))
+
+//Hot reload!
+//ALL server routes are in this module!
+
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+//...
+
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${port}`);
+});
