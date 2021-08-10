a9b821648a2d33e22f5347bc2a36c054140dc890
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2021-08-10 19:03:47 -0400

    Integrated Eslint, implemented basic Product Page.

--- compiler.js
+++ compiler.js
@@ -1,14 +1,16 @@
+/**
+ * Disabled because this is a development tool
+ */
+/* eslint-disable import/no-extraneous-dependencies, no-console */
 const fs = require('fs');
-const ejs = require("ejs");
+const ejs = require('ejs');
 const path = require('path');
 
-
-
-(async ()=>{
+(async () => {
   // Our starting point
   try {
     // Get the files as an array
-    const files = await fs.promises.readdir(path.join(__dirname, 'views', 'pages') );
+    const files = await fs.promises.readdir(path.join(__dirname, 'views', 'pages'));
 
     // Loop them all with the new for...of
     files.forEach(async (file) => {
@@ -16,21 +18,19 @@ const path = require('path');
       const stat = await fs.promises.stat(templatePath);
 
       // non product pages we can just compile without issue. Product page we need to load the products from JSON
-      if(stat.isFile()) {
-        const toPath = path.join(__dirname, 'public', 'views', `${file.split('.').slice(0,-1).join('')}.html`)
+      if (stat.isFile()) {
+        const toPath = path.join(__dirname, 'public', 'views', `${file.split('.').slice(0, -1).join('')}.html`);
         const templateStr = ejs.fileLoader(templatePath, 'utf8');
         const template = ejs.compile(templateStr, { filename: templatePath });
         const renderedHtml = template();
-        fs.writeFile(toPath, renderedHtml, function(err) {
-          if(err) { console.log(err); return false }
+        fs.writeFile(toPath, renderedHtml, (err) => {
+          if (err) { console.log(err); return false; }
           return true;
-        });  
+        });
       }
-    })
-  }
-  catch( e ) {
+    });
+  } catch (e) {
     // Catch anything bad that happens
-    console.error( "We've thrown! Whoops!", e );
+    console.error("We've thrown! Whoops!", e);
   }
-
 })();
--- index.js
+++ index.js
@@ -1,51 +1,59 @@
-const express = require("express");
+/**
+ * Disabled because this is a development tool
+ */
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+const express = require('express');
+
 const port = parseInt(process.env.PORT, 10) || 3000;
-const livereload = require("livereload");
-const connectLivereload = require("connect-livereload");
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
 const path = require('path');
 const fs = require('fs');
 
-const products = JSON.parse(fs.readFileSync('./products.json', {encoding:'utf8', flag:'r'}));
+const products = JSON.parse(fs.readFileSync('./products.json', { encoding: 'utf8', flag: 'r' }));
 /**
  * Live reload setup
  */
-const liveReloadServer = livereload.createServer()
+const liveReloadServer = livereload.createServer();
 liveReloadServer.watch([
   path.join(__dirname, 'views', 'pages'),
   path.join(__dirname, 'views', 'partials'),
 ]);
 
-liveReloadServer.server.once("connection", () => {
+liveReloadServer.server.once('connection', () => {
   setTimeout(() => {
-    liveReloadServer.refresh("/");
+    liveReloadServer.refresh('/');
   }, 100);
 });
 
 const app = express();
 app.set('view engine', 'ejs');
 
-
 // Use `.hbs` for extensions and find partials in `views/partials`.
 
 app.use(connectLivereload());
-app.use(express.static('views'))
+app.use(express.static('views'));
 
-//Hot reload!
-//ALL server routes are in this module!
+// Hot reload!
+// ALL server routes are in this module!
 
-
-app.get('/', function(req, res) {
-  
+app.get('/', (req, res) => {
   res.render('pages/index', {
     products,
   });
 });
-app.get('/about', function(req, res) {
+app.get('/about', (req, res) => {
   res.render('pages/index');
 });
-//...
+products.forEach((product) => {
+  app.get(product.endpoint, (req, res) => {
+    res.render('pages/productPage', {
+      product,
+    });
+  });
+});
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) throw err;
   console.log(`> Ready on http://localhost:${port}`);
 });
