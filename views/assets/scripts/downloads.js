188e3874d0ce9e5dd1eb74659e93b4d724435eef
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2021-08-14 23:27:34 -0400

    Visual bug fixed where dropdown accordion clipped bottom of page.

--- views/assets/scripts/downloads.js
+++ views/assets/scripts/downloads.js
@@ -6,13 +6,19 @@ const handleAccordionClick = (element) => {
     panel.style.boxShadow = null;
     panel.style.paddingTop = null;
     panel.style.paddingBottom = null;
+    panel.style.overflow = null;
+    panel.style.marginBottom = null;
     panel.setAttribute('data-toggled', false);
   } else {
     const additionalPadding = 32;
-    panel.style.maxHeight = `${panel.scrollHeight + additionalPadding}px`;
+    const remainingDistanceToBottomOfPage = window.innerHeight - panel.getBoundingClientRect().bottom - additionalPadding;
+    const scrollHeightWithPadding = panel.scrollHeight + additionalPadding;
+    panel.style.maxHeight = `${Math.min(remainingDistanceToBottomOfPage, scrollHeightWithPadding)}px`;
     panel.style.boxShadow = '0px 2px 2px 2px rgba(0,0,0, 0.1)';
     panel.style.paddingTop = '1em';
     panel.style.paddingBottom = '1em';
+    panel.style.overflow = 'auto';
+    panel.style.marginBottom = '4em';
     panel.setAttribute('data-toggled', true);
   }
 };
